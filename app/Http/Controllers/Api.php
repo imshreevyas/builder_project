f2f000cbb19b420414d572911e77de13cb5768c2
rohitvishin <rohitvishwakarma65@gmail.com>
2023-08-15 13:17:16 +0530

    update

--- app/Http/Controllers/Api.php
+++ app/Http/Controllers/Api.php
@@ -10,7 +10,23 @@ class Api extends Controller
 {
     //
 
-    
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('auth')->plainTextToken;
+
+            return response()->json([
+                'message' => 'success',
+                'user' => $user,
+                'token' => $token,
+            ],200);
+        }
+
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
     public function properties(){
         $data=Property::where('status',1)->with('documents')->get();
         if($data)
